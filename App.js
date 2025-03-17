d8c896512c5316cf25ecd7a071043c3398f8cdc6
Rifaz25 <rifazulk@gmail.com>
2025-03-18 00:29:02 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+//components
+import Login from './components/Accounts/login';
+function App() {
+  return (
+    <div className="App">
+      <login/>
+    </div>
+  );
+}
+
+export default App;
